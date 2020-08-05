cd6d0cd4715bf5606d6ff3a023e5ddec63b9fc6a
Andre Serudo <andreserudo@gmail.com>
2020-08-04 20:00:50 -0400

    rota da criacao de aulas

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesControllers';
+
+const routes = express.Router();
+
+const classesController = new ClassesController();
+
+routes.post('/classes', classesController.create);
+
+
+
+export default routes;
