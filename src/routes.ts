d75b6be463816eccb7750f7289c131302e300925
Andre Serudo <andreserudo@gmail.com>
2020-08-04 21:12:14 -0400

    finalizando a aula

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesControllers';
+import ConnectionsControllers from './controllers/ConnectionsControllers';
 
 const routes = express.Router();
 
 const classesController = new ClassesController();
+const connectionsControllers = new ConnectionsControllers();
+
 
 routes.post('/classes', classesController.create);
 
 routes.get('/classes', classesController.index);
+
+routes.post('/connections', connectionsControllers.create);
+
+routes.get('/connections', connectionsControllers.index);
 
 export default routes;
